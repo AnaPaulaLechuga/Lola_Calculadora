448e0d538a7380be5fddb073e0b18e90febc5ee9
La Lechuga <92046732+AnaPaulaLechuga@users.noreply.github.com>
2024-09-04 10:10:40 -0600

    Update ViewController.swift

--- CALCULADORA_LOLA/ViewController.swift
+++ CALCULADORA_LOLA/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var boton_para_interactuar: UIButton!
+    @IBOutlet weak var texto_a_cambiar: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    ///Descripcion
+    /// - Parameter sender: sender description
+    @IBAction func que_hacer_al_pushar_boton(_ sender: Any) {
+        texto_a_cambiar.text = (sender as UIButton)?.titleLabel
+        //boton_para_interactuar.titlelabel
+    }
 
 }
+
+
 
